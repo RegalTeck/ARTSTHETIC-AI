269a56dc78ed8cd0ea1dcb7941a65dc6b709c7a3
unknown <adewaleadeboye10@gmail.com>
2023-07-06 00:45:54 +0100

    third commit

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       screens: {
-        xs: '480px',
+        xm: "438px",
+        sm: "640px",
+        xmd: "750px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        tablet: "1480px",
+        laptop: "1720px",
       },
       fontFamily: {
-        inter: ['Inter var', 'sans-serif'],
+        inter: ["Inter var", "sans-serif"],
       },
       boxShadow: {
-        card: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)',
-        cardhover: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)',
+        card: "0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)",
+        cardhover:
+          "0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)",
       },
     },
   },
